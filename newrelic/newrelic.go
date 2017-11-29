04e0d3c8c3927ce90726fe9d1315cdf6c7254344
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2017-11-29 13:12:19 -0200

    Add NewRelic api client

---
+++ newrelic/newrelic.go
@@ -0,0 +1,73 @@
+package newrelic
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const defaultBaseURL = "https://api.newrelic.com/"
+
+type transport struct {
+	transport http.RoundTripper
+	apiKey    string
+}
+
+func (t *transport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("X-Api-Key", t.apiKey)
+	r.Header.Add("User-Agent", "newrelic_exporter;go")
+	return t.transport.RoundTrip(r)
+}
+
+// A Client manages communication with the NewRelic API
+type Client struct {
+	baseURL *url.URL
+	client  *http.Client
+}
+
+// NewClient returns an initialized NewRelic API client
+func NewClient(apiKey string) *Client {
+	baseURL, _ := url.Parse(defaultBaseURL)
+	return &Client{
+		baseURL: baseURL,
+		client: &http.Client{
+			Timeout: time.Second * 10,
+			Transport: &transport{
+				transport: http.DefaultTransport,
+				apiKey:    apiKey,
+			},
+		},
+	}
+}
+
+func (c *Client) newRequest(method, path string) (*http.Request, error) {
+	url, err := c.baseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, err
+}
+
+func (c *Client) do(req *http.Request, result interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer closeResponse(resp)
+
+	err = json.NewDecoder(resp.Body).Decode(result)
+	return resp, err
+}
+
+func closeResponse(resp *http.Response) {
+	if resp != nil {
+		resp.Body.Close()
+	}
+}
