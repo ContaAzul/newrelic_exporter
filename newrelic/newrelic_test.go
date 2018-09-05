b0e38f53a9c6aaa6a0f168548278febe55a462a3
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2018-09-05 10:10:50 -0300

    typo

--- newrelic/newrelic_test.go
+++ newrelic/newrelic_test.go
@@ -11,14 +11,14 @@ import (
 // responseStub fakes a NewRelic API response
 func responseStub(t *testing.T, filename string, status int) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bts := readTestdata(t, filename)
+		bts := readTestData(t, filename)
 		w.WriteHeader(status)
 		w.Write(bts)
 	}))
 }
 
-// readTestdata reads the file named by filename under the testdata directory and returns the contents
-func readTestdata(t *testing.T, filename string) []byte {
+// readTestData reads the file named by filename under the testdata directory and returns the contents
+func readTestData(t *testing.T, filename string) []byte {
 	path := filepath.Join("testdata", filename)
 	bts, err := ioutil.ReadFile(path)
 	if err != nil {
