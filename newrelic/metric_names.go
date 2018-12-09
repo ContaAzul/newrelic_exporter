483cc5affdaa06b01b12a35240aaf2a8bed81b11
willlie1 <willlie1@users.noreply.github.com>
2018-12-09 20:06:39 +0100

    Added method to retrieve apdex metric names

--- newrelic/metric_names.go
+++ newrelic/metric_names.go
@@ -1,5 +1,10 @@
 package newrelic
 
+import (
+	"fmt"
+	"github.com/prometheus/common/log"
+)
+
 type MetricNames struct {
 	Metrics []MetricName `json:"metrics"`
 }
@@ -7,4 +12,17 @@ type MetricNames struct {
 type MetricName struct {
 	Name   string   `json:"name"`
 	Values []string `json:"values"`
+}
+
+func (c *Client) ListApdexMetricNames(applicationID int64) ([]MetricName, error) {
+	log.Info("Getting apdex MetricNames")
+	path := fmt.Sprintf("v2/applications/%d/metrics.json?name=%s", applicationID, "Apdex")
+	req, err := c.newRequest("GET", path)
+	if err != nil {
+		return nil, err
+	}
+
+	var response MetricNames
+	_, err = c.do(req, &response)
+	return response.Metrics, err
 }
